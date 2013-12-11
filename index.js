39bcabd28f85259ae0b5e104365119673cc2e262
Michael Nisi <michael.nisi@gmail.com>
2013-12-11 18:49:31 +0100

    Node v0.11.x

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+
 // popfun - pop array, if last element is function
 
-module.exports = popfun
-
-function popfun (args) {
+module.exports = function (args) {
   if (!args || !args.length) return null
-  
+
   if (typeof args[args.length - 1] === 'function') {
     return args.pop()
   }
